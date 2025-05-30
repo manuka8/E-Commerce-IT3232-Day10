67aa47f8eb14389a210fde092ce300ec7375589f
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-05-30 23:32:45 +0530

    Create department repository

---
+++ day10/src/main/java/lk/ac/vau/fas/ict/Day10Application.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.fas.ict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day10Application.class, args);
+	}
+
+}
